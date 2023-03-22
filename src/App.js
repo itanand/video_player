7bd28aa46e9224aef267ef884d181f54d73439d9
Anand Mohan <anand1505mohan@gmail.com>
2023-03-23 01:04:35 +0530

    React Router Doms Installed and App.js Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Feed from "./components/Feed/Feed";
+import SearchResult from "./components/Search/SearchResult";
+import VideoDetails from "./components/Video/VideoDetails";
 import { AppContext } from "./context/contextApi";
+
 
 const App = () => {
   return (
     <AppContext>
-          <div>App</div>
-    </AppContext>
+            <BrowserRouter>
+                <div className="flex flex-col h-full">
+                    <Header />
+                    <Routes>
+                        <Route path="/" exact element={<Feed />} />
+                        <Route
+                            path="/searchResult/:searchQuery"
+                            element={<SearchResult />}
+                        />
+                        <Route path="/video/:id" element={<VideoDetails />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </AppContext>
 
   )
 }
