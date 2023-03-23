16c3d12ff606715916ebacabe008bed4b9cc65a7
Anand Mohan <anand1505mohan@gmail.com>
2023-03-23 21:59:48 +0530

    left

--- src/components/LeftNavMenuItem.jsx
+++ src/components/LeftNavMenuItem.jsx
@@ -4,7 +4,7 @@ const LeftNavMenuItem = ({ text, icon, className, action }) => {
     return (
         <div
             className={
-                "text-white text-sm cursor-pointer h-10 flex items-center px-3 mb-[1px] rounded-lg hover:bg-white/[0.15] " +
+                "text-white text-sm cursor-pointer h-10 flex items-center px-3 mb-[1px] rounded-lg hover:bg-red-600 " +
                 className
             }
             onClick={action}
